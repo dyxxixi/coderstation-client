d4efb28f1d5747c8e4e01bfc195555942ca2075b
d-yx <352104389@qq.com>
2023-10-04 18:52:25 +0800

    test:test vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), unocss()],
+  plugins: [react()],
   server: {
     proxy: {
       // '/res': 'https://coderstation-server.vercel.app',
